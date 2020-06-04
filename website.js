024d3ac756278165974e45e660d16fbfbb17f80d
Roma <moiezirbaz@gmail.com>
2020-06-04 21:49:24 +0500

    hoor-beauty-saloon

---
+++ website.js
@@ -0,0 +1,20 @@
+function Image() {
+document.getElementById("image-change").src='image/women-haircut.jpg'
+}
+
+
+function Image2() {
+document.getElementById("image-change").src='image/teen-haircut.jpg'
+}
+
+function Image3() {
+    document.getElementById("image-change").src='image/services-1.jpg'
+}
+
+function Image4() {
+    document.getElementById("image-change").src='image/makeup21.jpg'
+}
+
+function Image5() {
+    document.getElementById("image-change").src='image/makeup12.jpg'
+}
